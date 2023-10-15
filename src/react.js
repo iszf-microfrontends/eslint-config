228cabd7ef964727d0fb7601b8a660d0e740e00b
Sviatoslav <sviatoslavnazin@gmail.com>
2023-10-15 10:32:22 +0800

    add eslint-plugin-jsx-a11y

--- src/react.js
+++ src/react.js
@@ -7,6 +7,7 @@ module.exports = {
     ...['./base.js'].map(require.resolve),
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
     'plugin:prettier/recommended',
   ],
   rules: {
